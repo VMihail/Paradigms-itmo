738a51f696a37344cbfed91cdc8a80d54f2e7f31
VMihail <83347281+VMihail@users.noreply.github.com>
2023-02-10 22:07:03 +0300

    add contract

--- src/search/Utils.java
+++ src/search/Utils.java
@@ -1,6 +1,8 @@
 package search;
 
 public final class Utils {
+  // Pred: left >= 0 and right >= 0 and left + right <= Integer.MAX_VALUE
+  // Post: result == (left + right) / 2
   public static int getMiddle(int left, int right) {
     return ((right - left) >> 1) + left;
   }
